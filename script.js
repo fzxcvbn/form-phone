feddd05fb1c17696a5ddb640e4cce6844d083ad1
fzxcvbn <diankahid@gmail.com>
2025-05-16 23:30:39 +0300

    update

--- script.js
+++ script.js
@@ -6,13 +6,37 @@ const waitCallBtn = document.getElementById("wait-call-btn");
 
 const overlay = document.getElementById("overlay");
 
+const time = document.getElementById("datetime");
+
+const btnDatetime = document.getElementById("btn-datetime");
+
+const btnNoDatetime = document.getElementById("btn-no-datetime");
+
+btnDatetime.addEventListener("click", () => {
+  time.style.display = "block";
+});
+
+btnNoDatetime.addEventListener("click", () => {
+  time.style.display = "none";
+});
+
 deleteBtn.addEventListener("click", () => {
     popup.style.display = "block";
     overlay.classList.add("show");
 });
+
 document.getElementById('phoneForm').addEventListener('submit', function(e) {
     e.preventDefault();
     const phone = this.phone.value;
+    if (time.value) {
+      const valueTime = time.value;
+    } else {
+      const now = new Date();
+      const hours = now.getHours();
+      const minutes = now.getMinutes();
+      const seconds = now.getSeconds();
+      const valueTime = (`Текущее время: ${hours}:${minutes}:${seconds}`)
+    }
     
     const bitrixData = {
       domain: '21vcrm.bitrix24.ru',
@@ -25,8 +49,10 @@ document.getElementById('phoneForm').addEventListener('submit', function(e) {
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
         fields: {
-          TITLE: bitrixData.leadTitle,
-          PHONE: [{"VALUE": phone, "VALUE_TYPE": "WORK"}]
+          "TITLE": "Новая заявка",
+          "NAME": "Посетитель сайта",
+          "PHONE": [{"VALUE": phone, "VALUE_TYPE": "WORK"}],
+          "COMMENTS": "Желаемое время: " + valueTime
         }
       })
     })
